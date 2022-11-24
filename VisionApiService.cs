2fd209b91404ec81a6dbb0241543fa3451ccec31
Mdubaneh <65094648+Digital-101@users.noreply.github.com>
2022-11-25 05:18:51 +1400

    Add files via upload

---
+++ VisionApiService.cs
@@ -0,0 +1,94 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+using VisionApiDemo.Models;
+
+namespace VisionApiDemo.Business_Layer
+{
+    public class VisionApiService
+    {
+        const string subscriptionKey = "c968ef7e-57c7-4899-93de-61eabd9dbdfc";
+        const string uriBase =
+            "https://group14vision.cognitiveservices.azure.com/";
+
+        public async Task<ImageInfoViewModel> MakeAnalysisRequest()
+        {
+            string imageFilePath = @"C:\Users\User\Desktop\old.png";
+            var errors = new List<string>();
+            ImageInfoViewModel responeData = new ImageInfoViewModel();
+            try
+            {
+                HttpClient client = new HttpClient();
+
+                // Request headers.
+                client.DefaultRequestHeaders.Add(
+                    "Ocp-Apim-Subscription-Key", subscriptionKey);
+
+                // Request parameters. A third optional parameter is "details".
+                string requestParameters =
+                    "visualFeatures=Categories,Description,Color";
+
+                // Assemble the URI for the REST API Call.
+                string uri = uriBase + "?" + requestParameters;
+
+                HttpResponseMessage response;
+
+                // Request body. Posts a locally stored JPEG image.
+                byte[] byteData = GetImageAsByteArray(imageFilePath);
+
+                using (ByteArrayContent content = new ByteArrayContent(byteData))
+                {
+                    // This example uses content type "application/octet-stream".
+                    // The other content types you can use are "application/json"
+                    // and "multipart/form-data".
+                    content.Headers.ContentType =
+                        new MediaTypeHeaderValue("application/octet-stream");
+
+                    // Make the REST API call.
+                    response = await client.PostAsync(uri, content);
+                }
+
+                // Get the JSON response.
+                var result = await response.Content.ReadAsStringAsync();
+
+                if (response.IsSuccessStatusCode)
+                {
+
+                    responeData = JsonConvert.DeserializeObject<ImageInfoViewModel>(result,
+                        new JsonSerializerSettings
+                        {
+                            NullValueHandling = NullValueHandling.Include,
+                            Error = delegate (object sender, Newtonsoft.Json.Serialization.ErrorEventArgs earg)
+                            {
+                                errors.Add(earg.ErrorContext.Member.ToString());
+                                earg.ErrorContext.Handled = true;
+                            }
+                        }
+                    );
+                }
+
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("\n" + e.Message);
+            }
+
+            return responeData;
+        }
+
+        static byte[] GetImageAsByteArray(string imageFilePath)
+        {
+            using (FileStream fileStream =
+                new FileStream(imageFilePath, FileMode.Open, FileAccess.Read))
+            {
+                BinaryReader binaryReader = new BinaryReader(fileStream);
+                return binaryReader.ReadBytes((int)fileStream.Length);
+            }
+        }
+    }
+}
